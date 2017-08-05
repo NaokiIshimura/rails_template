6e3362036a45effe3ee2924f01eab1bb39757cb5
Naoki Ishimura <nao.king.go@gmail.com>
2017-08-05 18:54:29 +0900

    Fix static_pages

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
-  get 'help'    => 'static_pages#help' # help_path
+  get 'home'  => 'static_pages#home'
+  get 'about' => 'static_pages#about'
+  get 'help'  => 'static_pages#help'
 end
